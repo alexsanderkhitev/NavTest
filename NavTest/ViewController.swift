a124612d991d48e7944acfbdc478fd91b33c39e0
Alexsander Khitev <alexsanderskywork@gmail.com>
2016-08-20 21:34:39 +0400

    init

---
+++ NavTest/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  NavTest
+//
+//  Created by Alexsander Khitev on 8/20/16.
+//  Copyright © 2016 Alexsander Khitev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var image = UIImage() 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    var firstLoad = false
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.shadowImage = nil
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
